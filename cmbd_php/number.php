3b017aaacfa74b864517db92e56df968199d0e76
Akeed Anjum <42532302+Akeedo@users.noreply.github.com>
2019-04-29 18:19:08 +0600

    Add files via upload

---
+++ cmbd_php/number.php
@@ -0,0 +1,15 @@
+<?php 
+	$number = 12;
+	$number2 = 1.0001;
+	
+	 echo $checking = 1 + 2 - 2 * 5;
+	 echo "<br>";
+	 $a = 2**3;
+	 echo $a;
+	 echo "<br>";
+	echo floor($number2);
+	echo "<br>";
+	echo ceil($number2);
+	echo "<br>";
+	echo pi();
+?>
